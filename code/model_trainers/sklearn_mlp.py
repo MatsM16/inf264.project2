2313132e379b555993214887ca642d19f446dd33
MatsM16 <matdyr23@gmail.com>
2023-10-13 14:41:15 +0200

    Including dump files in git

--- code/model_trainers/sklearn_mlp.py
+++ code/model_trainers/sklearn_mlp.py
@@ -4,20 +4,13 @@ from plot import plot_accuracy_group
 
 def train_sklearn_mlp(X, y):
     hyper_parameters = [
-        (20, ), # Size
-        (20, 20), # Size x Size
-        (40,), # 2 Size
-        (40, 40), # 2 Size x 2 Size
-        (80,),# 4 Size
-        (80, 80), # 4 Size 4 2 Size
-        (80,),
-        (100,) # Default
+        (100,), # Default
         (100, 100),
         (100, 100, 100),
-        (100, 100, 100, 100),
-        (300, 300),
-        (300, 300, 300),
-        (300, 200, 100),
+        (400,),
+        (400,400),
+        (400,400,400),
+        (800, 400, 200),
     ]
 
     models = create_train_validate_model_group(
